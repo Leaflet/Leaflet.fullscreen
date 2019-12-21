e06bcfccc30fe2588d61a1642d09f32f3ae73582
runette <runette@gmail.com>
2019-12-21 16:37:53 +0200

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for leaflet-fullscreen 1.0.2
+// Project: https://github.com/Leaflet/Leaflet.fullscreen
+// Original Definitions by: Denis Carriere <https://github.com/DenisCarriere>
+// Updated by : Paul Harwood <https://github.com/runette>
+
+import {Control, ControlOptions} from 'leaflet';
+
+declare module 'leaflet' {
+    interface MapOptions {
+        fullscreenControl?: boolean | fullscreenOptions;
+    }
+
+    interface fullscreenOptions extends ControlOptions {
+        pseudoFullscreen?: boolean
+        title?: {
+            'false': string,
+            'true': string
+        }
+    }
+    namespace control {
+        function fullscreen(options: fullscreenOptions): Control.Fullscreen;
+        }
+    
+    namespace Control {
+        export class Fullscreen extends Control{
+            constructor (options: fullscreenOptions);
+            options: fullscreenOptions;
+        }
+    }
+
+}
